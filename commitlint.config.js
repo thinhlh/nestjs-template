4cdc43e58a30709f50833db0f835da2affe61e45
Jamie Le <jamie_le@mckinsey.com>
2024-01-18 19:24:56 +0700

    ci: update commit linting message pattern

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,11 +7,6 @@ module.exports = {
         }
     },
     rules: {
-        // "scope-case": [2, "always", "upper-case"],
-        // "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "ci"]],
-        // "type-empty": [2, "never"],
-        // "subject-empty": [2, "never"],
-        // "references-empty": [2, "never"],
         "header-max-length": [0, "always", 100],
     }
 };
